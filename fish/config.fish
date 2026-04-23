d7055ab712df0d840374c1cc09d6d76db624e50e
JoelBalzan <jbalzan.astro@gmail.com>
2026-04-23 21:31:21 +0800

    Switch to whitelist gitignore

--- fish/config.fish
+++ fish/config.fish
@@ -2,6 +2,9 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+alias oz="ssh -X jbalzan@nt.swin.edu.au"
+alias cira="ssh -X joel@134.7.50.144"
+
 set -U fish_greeting # Disable greeting
 set -gx TERM xterm-256color # 256 colors support
 set -gx EDITOR nvim # Default editor
